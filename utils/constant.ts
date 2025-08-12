3f369adef4b683a6f5c3de3b5687a3317dd6db71
yohannesgirmaw23@gmail.com <behaylugule@gmail.com>
2025-08-12 16:53:41 +0300

    test CI/CD

--- utils/constant.ts
+++ utils/constant.ts
@@ -8,6 +8,7 @@ export const USER_ROLE_OPTIONS = [
   { value: USER_ROLE.USER, label: 'User' },
 ];
 
+
 export const ORDER_STATUS_OPTION = [
   {value: ORDER_STATUS.PENDING, label:'pending'},
   {value:ORDER_STATUS.PROCESSING,label:'processing'},
